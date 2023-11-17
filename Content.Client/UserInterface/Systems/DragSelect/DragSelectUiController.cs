1545abc4ef5fcd83047ab973c18be796e4d686fc
Gubbinal Girl <GubbinalGirl@gmail.com>
2023-11-16 19:52:58 -0600

    Initial files.

---
+++ Content.Client/UserInterface/Systems/DragSelect/DragSelectUiController.cs
@@ -0,0 +1,7 @@
+using Robust.Client.UserInterface.Controllers;
+
+namespace Content.Client.UserInterface.Systems.DragSelect;
+
+public sealed class DragSelectUiController : UIController
+{
+}
---
+++ Content.Client/UserInterface/Systems/DragSelect/Overlays/DragSelectOverlay.cs
@@ -0,0 +1,7 @@
+using Robust.Client.Graphics;
+
+namespace Content.Client.UserInterface.Systems.DragSelect.Overlays;
+
+public sealed class DragSelectOverlay : Overlay
+{
+}
