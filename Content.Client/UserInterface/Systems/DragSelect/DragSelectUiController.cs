df30e446cbc2a6e0ce549ff2b57e68c66cb2a874
Gubbinal Girl <GubbinalGirl@gmail.com>
2023-11-16 20:58:37 -0600

    More work.

--- Content.Client/UserInterface/Systems/DragSelect/DragSelectUiController.cs
+++ Content.Client/UserInterface/Systems/DragSelect/DragSelectUiController.cs
@@ -2,6 +2,7 @@ using Robust.Client.Graphics;
 using Robust.Client.Input;
 using Robust.Client.Player;
 using Robust.Client.UserInterface.Controllers;
+using Robust.Shared.Map;
 
 namespace Content.Client.UserInterface.Systems.DragSelect;
 
@@ -9,6 +10,11 @@ public sealed class DragSelectUiController : UIController
 {
     [Dependency] private readonly IOverlayManager _overlayManager = default!;
     [Dependency] private readonly IEyeManager _eyeManager = default!;
+    [Dependency] private readonly IInputManager _inputManager = default!;
+
+
+    private MapCoordinates? _curStartCoords;
+    private MapCoordinates? _curEndCoords;
 
     private Overlays.DragSelectOverlay _overlay = default!;
     public override void Initialize()
@@ -17,12 +23,32 @@ public sealed class DragSelectUiController : UIController
         SubscribeLocalEvent<LocalPlayerAttachedEvent>(OnPlayerAttach);
         SubscribeLocalEvent<LocalPlayerDetachedEvent>(OnPlayerDetached);
         SubscribeLocalEvent<MouseButtonEventArgs>(OnMouseMove);
-        SubscribeLocalEvent<MouseButtonEventArgs>(OnMousePress);
+        SubscribeLocalEvent<MouseButtonEventArgs>(OnMouseButtonEvent);
     }
 
-    private void OnMousePress(MouseButtonEventArgs ev)
+    private void OnMouseButtonEvent(MouseButtonEventArgs ev)
     {
-        throw new NotImplementedException();
+        if (ev.Button != Mouse.Button.Left)
+            return;
+
+        //Is it pressed or released? Check Keyboard.Key.MouseLeft
+        if (_inputManager.IsKeyDown(Keyboard.Key.MouseLeft))
+        {
+            //It was pressed.
+            _curStartCoords = _eyeManager.ScreenToMap(ev.Position);
+            _curEndCoords = null;
+            //Clear the selected objects
+        }
+        else
+        {
+            //It was released.
+            if (_curStartCoords == null)
+                return;
+
+            _curEndCoords = _eyeManager.ScreenToMap(ev.Position);
+
+            GetSelectedObjects();
+        }
     }
 
     private void OnMouseMove(MouseButtonEventArgs ev)
@@ -38,5 +64,10 @@ public sealed class DragSelectUiController : UIController
     private void OnPlayerAttach(LocalPlayerAttachedEvent ev)
     {
         throw new NotImplementedException();
+    }
+
+    private void GetSelectedObjects()
+    {
+
     }
 }
