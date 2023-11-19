e28eff32d85a67e4ab02f5502cb08e9103d71d6e
Gubbinal Girl <GubbinalGirl@gmail.com>
2023-11-19 12:27:10 -0600

    Initial for SelectionBufferSystem. This system will manage remembering and acting upon a selection of entities.

---
+++ Content.Client/SelectionBuffer/SelectionBufferSystem.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Content.Client.SelectionBuffer;
+
+public sealed class SelectionBufferSystem : EntitySystem
+{
+    private HashSet<EntityUid> _selectedEntities = new HashSet<EntityUid>();
+
+    public void ClearSelection()
+    {
+        _selectedEntities.Clear();
+    }
+
+    public void AddToSelection(HashSet<EntityUid> entities)
+    {
+        _selectedEntities.UnionWith(entities);
+    }
+
+    public void RemoveFromSelection(HashSet<EntityUid> entities)
+    {
+        _selectedEntities.ExceptWith(entities);
+    }
+}
