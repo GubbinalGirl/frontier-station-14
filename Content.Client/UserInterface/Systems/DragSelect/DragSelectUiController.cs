e78239897769641c2b911639da417299ac19d155
Gubbinal Girl <GubbinalGirl@gmail.com>
2023-11-19 09:48:43 -0600

    Documentation.

--- Content.Client/UserInterface/Systems/DragSelect/DragSelectUiController.cs
+++ Content.Client/UserInterface/Systems/DragSelect/DragSelectUiController.cs
@@ -11,6 +11,9 @@ using System.Linq;
 
 namespace Content.Client.UserInterface.Systems.DragSelect;
 
+/// <summary>
+/// A UIController that allows players to select multiple objects and tiles by clicking and dragging the mouse.
+/// </summary>
 public sealed class DragSelectUiController : UIController
 {
     [Dependency] private readonly IOverlayManager _overlayManager = default!;
@@ -20,9 +23,14 @@ public sealed class DragSelectUiController : UIController
     [Dependency] private readonly IMapManager _mapManager = default!;
     private SharedMapSystem MapSystem => _entityManager.System<SharedMapSystem>();
 
+    /// <summary>
+    /// The point on screen where the player first left clicked. It is not converted to MapCoordinates
+    /// until the Use button is released so that the selection box can "move" with the player.
+    /// </summary>
     private ScreenCoordinates? _curStartCoords;
 
     private Overlays.DragSelectOverlay _overlay = default!;
+
     public override void Initialize()
     {
         base.Initialize();
@@ -59,6 +67,10 @@ public sealed class DragSelectUiController : UIController
         return false;
     }
 
+    /// <summary>
+    /// Every frame we update the overlay with the current mouse position. We expect non-relative coordinates.
+    /// </summary>
+    /// <param name="args"></param>
     public override void FrameUpdate(FrameEventArgs args)
     {
         //This is set when the Use action is pressed. If its null then we aren't drag selecting.
@@ -68,23 +80,29 @@ public sealed class DragSelectUiController : UIController
         _overlay.UpdateCoords(_curStartCoords, _inputManager.MouseScreenPosition);
     }
 
+    /// <summary>
+    /// Get the entities (excluding tiles) under the selection box.
+    /// </summary>
+    /// <param name="start">The point in screen space where the LMB was pressed.</param>
+    /// <param name="end">The point in screen space where the LMB was released.</param>
     private void GetSelectedObjects(ScreenCoordinates start, ScreenCoordinates end)
     {
-        // Find all the entities intersecting our click
-
-        //Covert to MapCoordinates
+        //Covert to MapCoordinates.
         MapCoordinates startMC = _eyeManager.ScreenToMap(start);
         MapCoordinates endMC = _eyeManager.ScreenToMap(end);
 
-        //Create a bounding box from these.
+        //Create a bounding box in world space from these.
         Box2 rect = new Box2(startMC.Position, endMC.Position);
 
-        //From GameplayStateBase::GetClickableEntities
+        //From GameplayStateBase::GetClickableEntities. Query the Sprite Tree for every object
+        //within the selection box.
         var spriteTree = _entityManager.EntitySysManager.GetEntitySystem<SpriteTreeSystem>();
         var entities = spriteTree.QueryAabb(startMC.MapId, rect);
 
+        //Print out the names of the selected entities for debug purposes.
         Logger.Debug(string.Format("Count: {0}", entities.Count));
 
+        //We get the entity name from the metadata component.
         var metadataQuery = _entityManager.GetEntityQuery<MetaDataComponent>();
 
         foreach (var e in entities)
@@ -98,15 +116,24 @@ public sealed class DragSelectUiController : UIController
         GetSelectedTiles(startMC, endMC);
     }
 
+    /// <summary>
+    /// Get the tiles under the selection box formed by the parameters. We pass MapCoordinates instead
+    /// of a Box2 because we need some extra data from the MapCoordinates class that wouldn't be in a Box2.
+    /// </summary>
+    /// <param name="start">The point where the LMB was pressed.</param>
+    /// <param name="end">The point where the LMB was released.</param>
     private void GetSelectedTiles(MapCoordinates start, MapCoordinates end)
     {
         if (_mapManager.TryFindGridAt(start.MapId, start.Position, out var gridUid, out var mapGrid))
         {
             Box2 box = new Box2(start.Position, end.Position);
+
             //We do not want to ignore empty tiles
             //GetTilesIntersecting is used instead of GetLocalTilesIntersecting because the former
             //allows us to use boxes that aren't in "top left to bottom right" form
             var tileRefs = MapSystem.GetTilesIntersecting(gridUid, mapGrid, box, false);
+
+            //Print some stuff for debug purposes.
             Logger.Debug(string.Format("Count of tileRefs: {0}", tileRefs.Count()));
 
             foreach (var t in tileRefs)
@@ -116,6 +143,9 @@ public sealed class DragSelectUiController : UIController
         }
     }
 
+    /// <summary>
+    /// Disables the overlay and clears the starting coordinates.
+    /// </summary>
     private void Clear()
     {
         _curStartCoords = null;
