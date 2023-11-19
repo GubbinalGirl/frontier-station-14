498bc9a0920ef76df39b824c4db80dd8e7612151
Gubbinal Girl <GubbinalGirl@gmail.com>
2023-11-18 21:29:38 -0600

    Prints out the name of highlighted entities. Doesn't select tiles.

--- Content.Client/UserInterface/Systems/DragSelect/DragSelectUiController.cs
+++ Content.Client/UserInterface/Systems/DragSelect/DragSelectUiController.cs
@@ -1,4 +1,4 @@
-using Content.Shared.Input;
+using Robust.Client.ComponentTrees;
 using Robust.Client.Graphics;
 using Robust.Client.Input;
 using Robust.Client.Player;
@@ -6,7 +6,6 @@ using Robust.Client.UserInterface.Controllers;
 using Robust.Shared.Input;
 using Robust.Shared.Input.Binding;
 using Robust.Shared.Map;
-using Robust.Shared.Player;
 using Robust.Shared.Timing;
 
 namespace Content.Client.UserInterface.Systems.DragSelect;
@@ -14,8 +13,9 @@ namespace Content.Client.UserInterface.Systems.DragSelect;
 public sealed class DragSelectUiController : UIController
 {
     [Dependency] private readonly IOverlayManager _overlayManager = default!;
-    [Dependency] private readonly IEyeManager _eyeManager = default!;
     [Dependency] private readonly IInputManager _inputManager = default!;
+    [Dependency] private readonly IEntityManager _entityManager = default!;
+    [Dependency] private readonly IEyeManager _eyeManager = default!;
 
     //Need to store the ScreenCoords as well
     private ScreenCoordinates? _curStartCoords;
@@ -68,6 +68,30 @@ public sealed class DragSelectUiController : UIController
     private void GetSelectedObjects(ScreenCoordinates start, ScreenCoordinates end)
     {
         //Do we select tiles or just entities?
+        // Find all the entities intersecting our click
+
+        //Covert to MapCoordinates
+        MapCoordinates startMC = _eyeManager.ScreenToMap(start);
+        MapCoordinates endMC = _eyeManager.ScreenToMap(end);
+
+        //Create a bounding box from these.
+        Box2 rect = new Box2(startMC.Position, endMC.Position);
+
+        //From GameplayStateBase::GetClickableEntities
+        var spriteTree = _entityManager.EntitySysManager.GetEntitySystem<SpriteTreeSystem>();
+        var entities = spriteTree.QueryAabb(startMC.MapId, rect);
+
+        Logger.Debug(string.Format("Count: {0}", entities.Count));
+
+        var metadataQuery = _entityManager.GetEntityQuery<MetaDataComponent>();
+
+        foreach (var e in entities)
+        {
+            if (metadataQuery.TryGetComponent(e.Uid, out var component))
+            {
+                Logger.Debug(string.Format("Entity name: {0}", component.EntityName));
+            }
+        }
 
     }
 
