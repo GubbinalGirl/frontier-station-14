83b05e1d7196630853d70ef4ac206bafc15e743c
Gubbinal Girl <GubbinalGirl@gmail.com>
2023-11-21 17:37:26 -0600

    Turned the List back to HashSet in the events.

--- Content.Server/SelectionBuffer/SelectionBufferSystem.cs
+++ Content.Server/SelectionBuffer/SelectionBufferSystem.cs
@@ -9,4 +9,5 @@ namespace Content.Server.SelectionBuffer;
 
 public sealed class SelectionBufferSystem : SharedSelectionBufferSystem
 {
+    //This class can remain empty. Its only needed so that a server side system is started.
 }
